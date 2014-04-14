f23d8aaa1c687398b8819dca218cc4cc55cb819e
Jim A Kinsman <jakinsman@gmail.com>
2014-04-14 08:54:17 -0500

    quick-commit-Mon Apr 14 08:54:17 CDT 2014

--- JKGesture.cpp
+++ JKGesture.cpp
@@ -13,6 +13,7 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 	{
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
+		Application->Title = "JKGesturePad";
 		Application->CreateForm(__classid(TfrmGesturePad), &frmGesturePad);
 		Application->CreateForm(__classid(TfrmSettings), &frmSettings);
 		Application->Run();
